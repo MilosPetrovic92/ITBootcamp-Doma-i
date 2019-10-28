dd055eaa72cab9b315e3c35ccde0ddc124123514
Miloš Petrović <Milo Petrovi>
2019-10-28 16:48:02 +0100

    Zadaci 1 i 2 za Dan 1.

---
+++ Nedelja2/Dan 1/Zadatak 2/index.js
@@ -0,0 +1,5 @@
+const promena="Welcome to DOM";
+document.getElementById('naslov').innerHTML = promena;
+
+document.getElementById('tekst').innerHTML = `<p>Beethoven tells you what it's like to be Beethoven and Mozart tells you what it's like to be human. Bach tells you what it's like to be the universe.”<br/>
+― Douglas Adams</p>`;
