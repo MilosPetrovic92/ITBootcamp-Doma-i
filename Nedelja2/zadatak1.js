049191fa6d54ba023e602f966de53176786c6296
Miloš Petrović <Milo Petrovi>
2019-10-30 00:11:18 +0100

    Brisanje foldera

---
+++ Nedelja2/zadatak1.js
@@ -0,0 +1,20 @@
+var a=20;
+var b=10;
+
+var sabiranje=a+b;
+console.log(`Zbir promenljivih a i b je ${sabiranje}.`);
+
+var oduzimanje=a-b;
+console.log(`Razlika promenljivih a i b je ${oduzimanje}.`);
+
+var množenje=a*b;
+console.log(`Proizvod promenljivih a i b je ${množenje}.`);
+
+var deljenje=a/b;
+console.log(`Količnik promenljivih a i b je ${deljenje}.`);
+
+var ostatak=a%b;
+console.log(`Ostatak promenljivih a i b je ${ostatak}.`);
+
+var stepenovanje=a**b;
+console.log(`Rezultat stepenovanja promenljivih a i b je ${stepenovanje}.`);
